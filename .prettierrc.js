484e24afc6196c2b0fb56b1695e449961dc5a4e1
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-02-28 19:54:45 +0700

    fix: add prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,6 @@ module.exports = {
   bracketSpacing: true,
   htmlWhitespaceSensitivity: 'ignore',
   insertPragma: false,
-  jsxBracketSameLine: false,
   jsxSingleQuote: false,
   printWidth: 80,
   proseWrap: 'preserve',
